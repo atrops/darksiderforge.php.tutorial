2e3527af3e3646408524f01e7ca76e298b7f83ad
darksider3 <leon@darksider3.de>
2013-01-27 17:35:34 +0100

    Created a Skript to copy the dokuwiki files into dokuwiki directory;HOWTO for this created

---
+++ copy_gitWiki_to_dokuWikiSpace2.pl
@@ -0,0 +1,23 @@
+my $_dkw="/var/www/dokuwiki-2012-10-13/";
+my $_dkwGit="/home/leon/Arbeitsfläche/dokuwiki_PAGES/";
+if(system("test -d ".$_dkw) == 0)
+{
+	print "dokuwiki directory found.\n";
+}
+else{
+	print "Dokuwiki directory not found! Exit";
+	exit(1);
+}
+if(system("test -d ".$_dkwGit)==0)
+{
+	print "Git Repo found.\n";
+}
+else{
+	print "Git Repo not found..exit";
+	exit(1);
+}
+system("sudo chmod 777 -R ".$dkwGit);
+system("cp -R -u -v ".$_dkwGit."media/ ".$_dkw."data/media/");
+system("cp -R -u -v ".$_dkwGit."media_meta/ ".$_dkw."data/media_meta/");
+system("cp -R -u -v ".$_dkwGit."php/ ".$_dkw."data/pages/php/");
+system("cp -u -v ".$_dkwGit."php.txt ".$_dkw."data/pages/php.txt");
